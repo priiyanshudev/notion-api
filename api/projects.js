94e0f8b83cedff05b8eaa986a117177249c6f162
Priyanshu Dev <Priyanshudev037@gmail.com>
2026-03-18 19:54:51 +0530

    Update projects.js

--- api/projects.js
+++ api/projects.js
@@ -1,6 +1,9 @@
 import { Client } from "@notionhq/client";
 
 export default async function handler(req, res) {
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
   try {
     const notion = new Client({ auth: process.env.NOTION_KEY });
 
@@ -9,8 +12,10 @@ export default async function handler(req, res) {
     });
 
     res.status(200).json(response.results);
+
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: error.message });
   }
+}
 }
