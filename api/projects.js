4f27d089f2670e8c601253ca3b948b32bfdbcacb
Priyanshu Dev <Priyanshudev037@gmail.com>
2026-03-18 19:28:43 +0530

    Update projects.js

--- api/projects.js
+++ api/projects.js
@@ -1,11 +1,16 @@
 import { Client } from "@notionhq/client";
 
 export default async function handler(req, res) {
-  const notion = new Client({ auth: process.env.NOTION_KEY });
+  try {
+    const notion = new Client({ auth: process.env.NOTION_KEY });
 
-  const response = await notion.databases.query({
-    database_id: process.env.DB_ID,
-  });
+    const response = await notion.databases.query({
+      database_id: process.env.DB_ID,
+    });
 
-  res.status(200).json(response.results);
+    res.status(200).json(response.results);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
 }
